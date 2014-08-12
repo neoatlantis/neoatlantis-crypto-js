aab462456351b26f39ee1076b9035b2d2f62ecef
neoatlantis <rijndael@yeah.net>
2014-08-13 00:03:14 +0200

    save

--- demo.js
+++ demo.js
@@ -1,1 +1,18 @@
 var crypto = require('./lib/enigma-jscrypto.js');
+function test(name, func){
+    var s = 'Testing item [' + name + '], result: ';
+    if(func())
+        s += 'OK';
+    else
+        s += 'FAILED!';
+    crypto.util.log.notice(s);
+};
+crypto.util.log.notice('----------------------');
+//////////////////////////////////////////////////////////////////////////////
+test('Random Generator', function(){
+    var x = new crypto.util.srand().bytes(1024);
+    return 1024 == x.byteLength;
+});
+test('Hash Function', function(){
+    
+});
