55cf505fd2a67f31f60344221a6bfce3f0747cad
neoatlantis <rijndael@yeah.net>
2014-08-13 00:29:36 +0200

    test sym. cipher

--- demo.js
+++ demo.js
@@ -13,6 +13,19 @@ test('Random Generator', function(){
     var x = new crypto.util.srand().bytes(1024);
     return 1024 == x.byteLength;
 });
+
 test('Hash Function', function(){
     
+});
+
+test('Symmetric Cipher Encryption and Decryption', function(){
+    var key = new crypto.util.srand().bytes(128);
+    var encryptor = new crypto.cipher.symmetric().key(key),
+        decryptor = new crypto.cipher.symmetric().key(key);
+    var plaintext = new crypto.util.srand().bytes(97);
+
+    var ciphertext = encryptor.encrypt(plaintext);
+    var decrypted = decryptor.decrypt(ciphertext);
+
+    return crypto.util.buffer.equal(decrypted, plaintext);
 });
