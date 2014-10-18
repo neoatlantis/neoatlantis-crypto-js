3ff08af28385edd26ef472f9f00e229ac9fff778
neoatlantis <rijndael@yeah.net>
2014-10-18 20:10:54 +0200

    save

--- demo.js
+++ demo.js
@@ -1,3 +1,27 @@
-var crypto = require('./lib/enigma-jscrypto.js');
+function translate(){
+    return {
+        error: function(v){return v},
+        question: function(v){return v},
+    }
+};
 
+/****************************************************************************/
+var argv = process.argv;
+var crypto = require('./lib/enigma-jscrypto.js'),
+    localStorage = require('node-persist');
 
+localStorage.initSync({
+    dir: './test',
+    continous: true,
+    interval: false,
+});
+
+var apiName = argv[2];
+
+var enigma = crypto.enigma.interface({
+    storage: localStorage,
+    translator: translate,
+});
+
+var session = enigma(apiName);
+console.log(session);
