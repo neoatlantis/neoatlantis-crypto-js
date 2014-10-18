6f176e0e319f876658e39e059593c00a303a7c97
neoatlantis <rijndael@yeah.net>
2014-10-18 22:49:54 +0200

    add demo tester

--- demo.js
+++ demo.js
@@ -1,11 +1,8 @@
-function translate(){
-    return {
-        error: function(v){return v},
-        question: function(v){return v},
-    }
-};
+var translate = {
+    error: function(v){return v},
+    question: function(v){return v},
+}
 
-/****************************************************************************/
 var argv = process.argv;
 var crypto = require('./lib/enigma-jscrypto.js'),
     localStorage = require('node-persist');
@@ -18,10 +15,36 @@ localStorage.initSync({
 
 var apiName = argv[2];
 
+
+
 var enigma = crypto.enigma.interface({
     storage: localStorage,
     translator: translate,
 });
 
 var session = enigma(apiName);
-console.log(session);
+
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+
+session.onQuestion(function(question){
+    rl.question(question.description, function(answer){
+        session.answer(answer);
+    });
+});
+
+session.onError(function(error){
+    console.log(error.description);
+});
+
+session.onTerminated(function(){
+    console.log('session terminated.');
+});
+
+
+session.start();
